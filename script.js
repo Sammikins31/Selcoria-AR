15b655da2761f99c019d0436a9d1d8a21cb7c37e
Sammikins31 <146858081+Sammikins31@users.noreply.github.com>
2025-04-09 10:21:46 +0100

    changes

--- script.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent('start-mindar', {
-    init: function () {
-      const sceneEl = this.el;
-      sceneEl.addEventListener('loaded', () => {
-        sceneEl.components['mindar-image'].start();
-      });
-    }
-  });
